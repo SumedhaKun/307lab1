a61034aaaabee08db8681d217ac652a8b4de720c
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-09-29 21:16:06 -0700

    added add user functionality

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,27 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table"
+import Form from "./Form";
 
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor"
-        } 
-      ]);
+    const [characters, setCharacters] = useState([]);
+    function updateList(person) {
+        setCharacters([...characters, person]);
+      }
       function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
           return i !== index;
         });
         setCharacters(updated);
       }
-    return (
+      return (
         <div className="container">
-          <Table characterData={characters} 
-          removeCharacter={removeOneCharacter}
+          <Table
+            characterData={characters}
+            removeCharacter={removeOneCharacter}
           />
-        </div>
-    );
+          <Form handleSubmit={updateList} />
+        </div>)
 }
 export default MyApp;
