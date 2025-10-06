cef0595a827e713dbda10ff5cb9a8de0e4aece68
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2025-10-05 18:49:44 -0700

    added delete and post endpoints

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -56,6 +56,47 @@ const findUserByName = (name) => {
       res.send(users);
     }
   });
+
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const removeUser = (user) => {
+  let index = users["users_list"].indexOf(user)
+  users["users_list"].splice(index,1);
+
+}
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; 
+  let user = findUserById(id);
+  console.log(user);
+  removeUser(user);
+  if (findUserById(id) === undefined) {
+    res.send(result);
+  }
+})
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
